3cf8cd6e773b30b1f1ce938760a7e512fa2631ce
cole <colehartman00@gmail.com>
2024-10-01 12:58:36 -0700

    prog-3

---
+++ Prog-3/generate.cpp
@@ -0,0 +1,8 @@
+// Name: Cole Hartman
+// Class (CECS 325-02)
+// Project Name (Prog 3 – Sorting Contest)
+// Due Date 10/15/24
+//
+// I certify that this program is my own original work. I did not copy any part
+// of this program from any other source. I further certify that I typed each
+// and every line of code in this program.
