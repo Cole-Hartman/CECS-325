6852da69d02a114931e90ebc6d36e7b481c71a6c
cole <colehartman00@gmail.com>
2024-10-03 11:46:27 -0700

    build: u

--- Prog-3/generate.cpp
+++ Prog-3/generate.cpp
@@ -11,16 +11,13 @@
 #include <iostream> // input output stream
 using namespace std;
 
-// generate a file called numbers.dat
-// numbers.dat will have COUNT random numbers between MIN and MAX.
-// generate COUNT MIN MAX
-// If there are not 3 cli arguments, the program fails and prints the error
+// generate random numbers and create numbers.dat
 
 // argc: # of arguments
 // argv: the arguments
 int main(int argc, char *argv[]) {
   if (argc != 4) {
-    cout << "Please enter 6 parameters:\n";
+    cout << "Please enter 3 parameters:\n";
     cout << "    Parameter 1: total count of numbers you want.\n";
     cout << "    Parameter 2: min value.\n";
     cout << "    Parameter 3: max value.\n";
@@ -39,8 +36,10 @@ int main(int argc, char *argv[]) {
   ofstream fout;
   fout.open("numbers.dat", ios::out);
   for (int i = 0; i < COUNT; i++) {
+    // generate the random numbers
     fout << (rand() % (MAX - MIN + 1) + MIN) << endl;
   }
+
   fout.close();
   return 0;
 }
